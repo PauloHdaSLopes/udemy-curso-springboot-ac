30bc3bdcc47f6866507374de27436d5cd210dff1
Paulo Lopes <paulohdas.lopes@gmail.com>
2022-03-05 00:38:55 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/home/cursomc/services/PedidoService.java
+++ src/main/java/com/home/cursomc/services/PedidoService.java
@@ -4,6 +4,9 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.home.cursomc.domain.Cliente;
@@ -14,6 +17,8 @@ import com.home.cursomc.domain.enums.EstadoPagamento;
 import com.home.cursomc.repositories.ItemPedidoRepository;
 import com.home.cursomc.repositories.PagamentoRepository;
 import com.home.cursomc.repositories.PedidoRepository;
+import com.home.cursomc.security.UserSS;
+import com.home.cursomc.services.excepts.AuthorizationException;
 import com.home.cursomc.services.excepts.ObjectNotFoundException;
 
 @Service
@@ -65,5 +70,16 @@ public class PedidoService {
 		itemPedidoRepo.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) { 
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		System.out.println("**CLIENTE*** " + cliente);
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
